9cf1ddb1df20cbc71232a00b8c03d00103172754
Vlad <vlad14196@gmail.com>
2023-02-23 09:43:41 +0300

    Initial Commit

---
+++ Firebase/FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseApp.swift
+//  Firebase
+//
+//  Created by user on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
